1865a4344236b8f9d1a0f26c7c408b33da195e48
AceDev28 <Trinhtheanh789@Gmail.com>
2022-02-23 10:44:32 +0700

    Add footer

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -550,22 +550,19 @@
                 </div>
                 <!-- /.container-fluid -->
 
+                <div>
+                    @include("Layout.Common.footer")
+                </div>
             </div>
             <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+
 
         </div>
+
     </div>
     <!-- End of Page Wrapper -->
     @include("Layout.Common.bottom_script")
+
 </body>
 </html>
--- resources/views/Layout/Common/footer.blade.php
+++ resources/views/Layout/Common/footer.blade.php
@@ -1,28 +1,9 @@
-{{-- <footer class="footer footer-transparent footer-main">
-    <div class="container">
-        <nav class="pull-left">
-            <ul>
-                <li>
-                    <a href="">
-                        Student Tuition Assistance Management Program - S.T.A.M.P
-                    </a>
-                </li>
-
-            </ul>
-        </nav>
-        <div class="social-area pull-right">
-            <a class="btn btn-social btn-twitter btn-simple" ">
-                <i class="fa fa-twitter"></i>
-            </a>
-            <a class="btn btn-social btn-facebook btn-simple" ">
-                <i class="fa fa-facebook-square"></i>
-            </a>
-            <a class="btn btn-social btn-google btn-simple" >
-                <i class="fa fa-google-plus"></i>
-            </a>
-        </div>
-        <div class="copyright">
-            &copy; 2021 | H.H.TA Group
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
         </div>
     </div>
-</footer> --}}
+</footer>
+<!-- End of Footer -->
