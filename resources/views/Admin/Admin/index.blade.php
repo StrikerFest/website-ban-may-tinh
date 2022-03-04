673373c1418d32da0d34914e67c6e1bd8ad127c6
AceDev28 <Trinhtheanh789@Gmail.com>
2022-03-04 16:42:18 +0700

    kewk

--- resources/views/Admin/Admin/index.blade.php
+++ resources/views/Admin/Admin/index.blade.php
@@ -53,7 +53,7 @@
 
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Thêm Admin mới</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
