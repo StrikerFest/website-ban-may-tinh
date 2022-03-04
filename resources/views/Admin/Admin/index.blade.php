a051c81020d2eb9d874c9b8d0a853f274093c89e
AceDev28 <Trinhtheanh789@Gmail.com>
2022-03-04 16:02:28 +0700

    Tạo controller Admin Tạo logic hiển thị Admin Tạo view hiển thị danh sách Admin

---
+++ resources/views/Admin/Admin/index.blade.php
@@ -0,0 +1,101 @@
+<html lang="en">
+<head>
+    @include("Admin.Layout.Common.meta")
+</head>
+<body>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        @include("Admin.Layout.Common.side_nav_menu")
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            @include("Admin.Layout.Common.header")
+            <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Administrator</h1>
+                    <p class="mb-4">Trang thông tin Admin.</p>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Bảng Admin hiện tại</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Tên</th>
+                                            <th>Email</th>
+                                            <th>Mật khẩu</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+
+                                    </tfoot>
+                                    <tbody>
+                                    @foreach ($admin as $A)
+
+                                        <tr>
+                                            <td>{{$A->tenND}}</td>
+                                            <td>{{$A->emailND}}</td>
+                                            <td>{{$A->matKhauND}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <form class="user">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
+                                            placeholder="First Name">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control form-control-user" id="exampleLastName"
+                                            placeholder="Last Name">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="Email Address">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleRepeatPassword" placeholder="Repeat Password">
+                                    </div>
+                                </div>
+                                <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                    Add data
+                                </a>
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+        </div>
+
+    </div>
+    <!-- End of Page Wrapper -->
+    @include("Admin.Layout.Common.bottom_script")
+
+</body>
+</html>
