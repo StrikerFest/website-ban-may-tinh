142f6e1e0f537d5758f65ef812ea819b3221b5bf
HuyHoang01 <vuhuyhoang310@gmail.com>
2022-03-05 13:23:42 +0700

    lại sửa migrations

---
+++ database/migrations/2022_03_04_172737_thongso.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thong_so', function(Blueprint $table){
+            $table->increments('maTS');
+            $table->string('tenTS', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thong_so');
+    }
+};
---
+++ database/migrations/2022_03_04_173512_sanpham_thongso.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('san_pham_thong_so', function(Blueprint $table){
+            $table->unsignedInteger('maSP');
+            $table->foreign('maSP')->references('maSP')->on('san_pham');
+            $table->unsignedInteger('maTS');
+            $table->foreign('maTS')->references('maTS')->on('thong_so');
+            $table->string('giaTri', 100);
+            $table->unique(['maSP', 'maTS']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('san_pham_thong_so');
+    }
+};
