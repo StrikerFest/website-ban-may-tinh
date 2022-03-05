88353d01a5a53e5406816a5873907d125dd3dc2d
AceDev28 <Trinhtheanh789@Gmail.com>
2022-03-05 16:06:09 +0700

    Comment

--- resources/views/Admin/Role/index.blade.php
+++ resources/views/Admin/Role/index.blade.php
@@ -49,6 +49,7 @@
                         </div>
                     </div>
 
+                    {{-- Form thêm chức vụ mới --}}
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold text-primary">Thêm Chức vụ mới</h6>
