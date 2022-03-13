aa806278672848aa7239913dd64b1f680ab2678e
AceDev28 <Trinhtheanh789@Gmail.com>
2022-03-13 14:36:44 +0700

    COMIT

--- resources/views/Customer/Customer/index.blade.php
+++ resources/views/Customer/Customer/index.blade.php
@@ -201,7 +201,7 @@
                                                                     <!-- Thẻ sale trên đầu -->
                                                                     <div class="badge bg-dark text-white position-absolute"
                                                                         style="top: 0.5rem; right: 0.5rem">
-                                                                        Sale
+                                                                        Sale!
                                                                     </div>
                                                                     {{-- Overlay hiển thị chi tiết sau khi hover --}}
                                                                     <div class="product-overlay">
