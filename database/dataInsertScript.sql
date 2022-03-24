8166334f7a43acab8569cd960b275b6ef08a3a34
HuyHoang <huyhoang@media-one.vn>
2022-03-24 11:29:05 +0700

    thêm sđt+tên người nhận

--- database/dataInsertScript.sql
+++ database/dataInsertScript.sql
@@ -65,13 +65,20 @@ INSERT INTO nha_san_xuat(`tenNSX`) VALUE ('Microsoft');
 
 -- =============================== ===========================================================================
 -- Bảng Thể loại
-INSERT INTO the_loai(`tenTL`) VALUE ('PC văn phòng');
-INSERT INTO the_loai(`tenTL`) VALUE ('PC gaming');
-INSERT INTO the_loai(`tenTL`) VALUE ('PC trạm');
+INSERT INTO the_loai(`tenTL`) VALUE ('PC');
 INSERT INTO the_loai(`tenTL`) VALUE ('Màn hình');
 INSERT INTO the_loai(`tenTL`) VALUE ('Ổ cứng');
-INSERT INTO the_loai(`tenTL`) VALUE ('Laptop gaming');
-INSERT INTO the_loai(`tenTL`) VALUE ('Laptop văn phòng');
+INSERT INTO the_loai(`tenTL`) VALUE ('Laptop');
+
+-- =============================== ===========================================================================
+-- Bảng Thể loại con
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (1, 'PC văn phòng');
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (1, 'PC gaming');
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (1, 'PC trạm');
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (2, 'Màn hình A');
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (3, 'Ổ cứng A');
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (4, 'Laptop gaming');
+INSERT INTO the_loai_con(`maTL`, `tenTLC`) VALUE (4, 'Laptop văn phòng');
 
 -- ==========================================================================================================
 -- Bảng Trạng thái sản phẩm
@@ -82,28 +89,42 @@ INSERT INTO tinh_trang_san_pham(`tenTTSP`) VALUE ('Liên hệ');
 
 -- ==========================================================================================================
 -- Bảng Sản phẩm
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Acer Gaming Predator Triton 500SE (PT516-51s-71RW) (NH.QAKSV.001) (i7 11800H/64GB RAM/1TB SSD/RTX 3080 8G/16.0 inch WQXGA 165Hz 100%sRGB/Win10/Xám) (2021)',82990000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Acer Gaming Predator Helios 500 PH517-52-797L (NH.QD3SV.001) (i711800H/64GB Ram/2TB SSD/RTX3080 8G/17.3 inch FHD 360Hz/Win 10/Đen)',98000000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Acer Gaming Predator Helios 300 PH315-54-74RU (NH.QC1SV.002) (i7 11800H/16GB Ram/512GB SSD/RTX3070 8G/15.6 inch QHD 165Hz/Win 10/Đen) (2021)',42000000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Acer Gaming Aspire 7 A715-42G-R4XX (NH.QAYSV.008) (R5 5500U/8GB RAM/256GB SSD/15.6 inch FHD/GTX1650 4G/Win11/Đen) (2021)',18000000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Acer Gaming Nitro 5 Eagle AN515-57-54MV (NH.QENSV.003) (i5 11400H/8GB Ram/512GB SSD/RTX3050 4G/15.6 inch FHD 144Hz/Win 11 mới nhất/Đen) (2021)',23000000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Gigabyte Gaming AORUS 15P (XD-73S1224GH) (i7 11800H /16GB Ram/1TB SSD/RTX3070 8G/15.6 inch FHD 240Hz/Win 10/Đen/Balo Aorus) (2021)',50000000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('Laptop Asus Gaming TUF FX706HCB-HX105W (i5 11400H/8GB RAM/512GB SSD/17.3 FHD 144hz/RTX 3050 4GB/Win11/Đen)',23000000,'Máy tính lập trình',28,2000000,1,6,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('PC GAMING HACOM LIAN-LI O11DX LIMITED (I7 12700KF/Z690/32GB RAM/1TB SSD/RTX 3070TI/1050W)',70000000,'Máy tính lập trình',28,2000000,1,2,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('PC GAMING HACOM PRO 020 (I5 11400F/B560/16GB RAM/500GB SSD/RTX 2060/650W)',30000000,'Máy tính lập trình',28,2000000,1,2,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('C GAMING HACOM 031 (I3 10105F/H510/8GB RAM/500GB SSD/GTX 1650/700W)',13000000,'Máy tính lập trình',28,2000000,1,2,1);
-INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTL`,`maTTSP`)
+INSERT INTO san_pham(`tenSP`,`giaSP`,`moTa`,`soLuong`,`giamGia`,`maNSX`,`maTLC`,`maTTSP`)
 VALUE('PC GAMING HACOM PRO 021 (R5 5600G/B550/16GB RAM/250GB SSD/RADEON RX VEGA/650W)',16900000,'Máy tính lập trình',28,2000000,1,2,1);
+
+-- ==========================================================================================================
+-- Bảng Khuyến mãi
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (1, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (2, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (3, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (4, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (5, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (6, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (7, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (8, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (9, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (10, 'Sản phẩm chưa có khuyến mãi');
+INSERT INTO khuyen_mai(`maSP`, `khuyenMai`) VALUE (11, 'Sản phẩm chưa có khuyến mãi');
 
 -- Bang Anh san pham
 INSERT INTO anh_san_pham(`maSP`,`anh`) VALUE (1,'60634_laptop_acer_gaming_predator_triton_500se_10.png');
